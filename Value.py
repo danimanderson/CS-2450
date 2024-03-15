df18a4cbc5c6878ae597bf1579fb21f162b3bd19
Cole Holbrook <coleholb@gmail.com>
2024-03-15 14:36:15 -0600

    New classes

---
+++ Value.py
@@ -0,0 +1,15 @@
+class Value:
+    def __init__(self, val = "+0000"):
+        self._val = val
+
+    def operator(self):
+        return self._val[1:3]
+    
+    def operand(self):
+        return self._val[3:]
+    
+    def get_val(self):
+        return self._val
+    
+    def set_val(self, new):
+        self._val = new
